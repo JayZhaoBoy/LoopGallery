30b189f60d2704d9302b9021520531ca7b447ad7
JayZhao <jayzhaoboy@gmail.com>
2018-02-22 21:29:50 +0800

    添加demo

--- app/src/main/java/com/example/jayzhao/loopgallery/MainActivity.java
+++ app/src/main/java/com/example/jayzhao/loopgallery/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.jayzhao.loopgallery;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.loop_gallery_vp.LoopGalleryByViewPager;
+import com.example.loop_gallery_vp.WrapperAdapterListener;
 
 public class MainActivity extends AppCompatActivity {
+    private LoopGalleryByViewPager mLoopGalleryByViewPager;
+    private int[] imgIds = {R.mipmap.cat1, R.mipmap.cat2, R.mipmap.cat3, R.mipmap.cat4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        mLoopGalleryByViewPager = (LoopGalleryByViewPager) findViewById(R.id.viewPager);
+        mLoopGalleryByViewPager.setAdapter(new MyPagerAdapter());
+    }
+
+    class MyPagerAdapter extends PagerAdapter implements WrapperAdapterListener {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_content, container, false);
+            ImageView imageView = (ImageView) view.findViewById(R.id.img);
+            Button button = (Button) view.findViewById(R.id.button);
+
+            imageView.setImageResource(imgIds[position]);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getRightCount() {
+            return imgIds.length;
+        }
+
+        @Override
+        public View getRightView(ViewGroup viewGroup, int position) {
+            return (View) instantiateItem(viewGroup, position);
+        }
     }
 }
--- loop_gallery/src/main/java/com/example/loop_gallery_vp/LoopGalleryByViewPager.java
+++ loop_gallery/src/main/java/com/example/loop_gallery_vp/LoopGalleryByViewPager.java
@@ -6,7 +6,9 @@ import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -19,6 +21,9 @@ import android.widget.FrameLayout;
 public class LoopGalleryByViewPager extends FrameLayout {
     //是否支持循环，默认为true
     private boolean isLoop = true;
+    private int mDefaultPageIndex = 1;
+    private ViewPager mViewPager;
+    private OnItemClickListener mItemClickListener;
 
     public LoopGalleryByViewPager(@NonNull Context context) {
         super(context);
@@ -35,21 +40,85 @@ public class LoopGalleryByViewPager extends FrameLayout {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        if (attrs == null){
+        setClipChildren(false);
+        if (attrs == null) {
             return;
         }
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.LoopGalleryByViewPager);
         isLoop = array.getBoolean(R.styleable.LoopGalleryByViewPager_isLoop, true);
+        mViewPager = new ViewPager(context);
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+        this.addView(mViewPager, params);
     }
 
-    private void setAdapter(PagerAdapter aAdapter) {
+    public void setAdapter(PagerAdapter aAdapter) {
         if (!(aAdapter instanceof WrapperAdapterListener)) {
             throw new IllegalArgumentException("adapter must implement WrapperAdapterListener");
         }
         LoopGalleryByViewPagerAdapter loopGalleryByViewPagerAdapter = new LoopGalleryByViewPagerAdapter();
         loopGalleryByViewPagerAdapter.setAdapter(aAdapter);
+        mViewPager.setAdapter(loopGalleryByViewPagerAdapter);
+        if (mDefaultPageIndex == 0 && isLoop){  //循环的情况下默认选中第二页，因为第一页是最后一项
+            mDefaultPageIndex = 1;
+        }
+        mViewPager.setCurrentItem(mDefaultPageIndex, false);
     }
 
+    /**
+     * 添加页面滑动监听
+     * @param listener
+     */
+    public void addOnPageChangeListener(final ViewPager.OnPageChangeListener listener) {
+        if (listener == null || mViewPager == null) {
+            return;
+        }
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                int newPosition = getRightPosition(position);
+                listener.onPageScrolled(newPosition, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                int newPosition = getRightPosition(position);
+                listener.onPageSelected(newPosition);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                listener.onPageScrollStateChanged(state);
+            }
+        });
+
+    }
+
+
+    /**
+     * 计算返回可用的position
+     * @return
+     */
+    private int getRightPosition(int originPosition) {
+        if (!isLoop || originPosition < 0) {
+            return originPosition;
+        }
+        int count = mViewPager.getChildCount();
+        int newPosition = 0;
+        if (originPosition == 0) {
+            newPosition = count - 3;
+        } else if (originPosition == count - 1) {
+            newPosition = 0;
+        } else {
+            newPosition = originPosition - 1;
+        }
+        return newPosition;
+    }
+
+
+    /**
+     * 包装adapter
+     */
     class LoopGalleryByViewPagerAdapter extends PagerAdapter {
         private PagerAdapter adapter;
 
@@ -71,43 +140,59 @@ public class LoopGalleryByViewPager extends FrameLayout {
 
         @Override
         public Object instantiateItem(ViewGroup container, final int position) {
+            int count = getCount();
             View newView = null;
-            if (!(adapter instanceof WrapperAdapterListener)){
+            if (!(adapter instanceof WrapperAdapterListener)) {
                 return newView;
             }
-            if (isLoop){
-                if (position == 0){
-                    newView = ((WrapperAdapterListener) adapter).getRightView(container, getCount() - 1);
-                }else if (position == getCount() - 1){
+            if (isLoop) {
+                if (position == 0) {
+                    newView = ((WrapperAdapterListener) adapter).getRightView(container, count - 3);
+                } else if (position == count - 1) {
                     newView = ((WrapperAdapterListener) adapter).getRightView(container, 0);
-                }else {
-                    newView = ((WrapperAdapterListener) adapter).getRightView(container, position);
+                } else {
+                    newView = ((WrapperAdapterListener) adapter).getRightView(container, position - 1);
                 }
-            }else {
+            } else {
                 newView = ((WrapperAdapterListener) adapter).getRightView(container, position);
             }
             newView.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (mItemClickListener != null){mItemClickListener.click(view, position);}
+                    if (mItemClickListener != null) {
+                        mItemClickListener.click(view, position);
+                    }
                 }
             });
+            container.addView(newView);
             return newView;
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            super.destroyItem(container, position, object);
+            container.removeView((View) object);
         }
     }
 
-    private OnItemClickListener mItemClickListener;
+    public int getDefaultPageIndex() {
+        return mDefaultPageIndex;
+    }
+
+    public void setDefaultPageIndex(int defaultPageIndex) {
+        mDefaultPageIndex = defaultPageIndex;
+        if (mViewPager != null) {
+            mViewPager.setCurrentItem(mDefaultPageIndex, false);
+        }
+    }
 
     public void setItemClickListener(OnItemClickListener itemClickListener) {
         mItemClickListener = itemClickListener;
     }
 
-    public interface OnItemClickListener{
+    /**
+     * 点击事件传递接口
+     */
+    public interface OnItemClickListener {
         void click(View view, int position);
     }
 
